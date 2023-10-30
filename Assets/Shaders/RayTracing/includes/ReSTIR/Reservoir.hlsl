74f20df5cc460aa9dba3dbab999a2a90e17ab50f
Tsukimi <hrj1125@qq.com>
2023-10-30 19:39:49 +0800

    feat: reservoir

--- Assets/Shaders/RayTracing/includes/ReSTIR/Reservoir.hlsl
+++ Assets/Shaders/RayTracing/includes/ReSTIR/Reservoir.hlsl
@@ -2,4 +2,68 @@
 #define KAISER_RAYTRACING_RESERVOIR
 
 
+struct GIReservoir
+{
+    float3 vPosition;           ///< Visible point's position.
+    float3 vNormal;          ///< Visible point's normal.
+    float3 sPosition;                ///< Hit point's position.
+    float3 sNormal;                  ///< Hit point's normal.
+    float3 radiance;                ///< Chosen sample's radiance.
+    float3 random;
+    int M;                       ///< Input sample count.
+    float avgWeight;
+    uint age;
+}
+
+struct PackedGIReservoir
+{
+    uint4 vPack;         ///< Visible point's position and normal.
+    uint4 sPack;              ///< Hit point's position and normal.
+    uint4 lightInfo;                ///< Reservoir information.
+
+};
+
+
+struct Reservoir
+{
+    float3 dir;
+    float w;
+    float wSum;
+    int M;
+    float randOffset;
+    int sampleIndex;
+
+    float rand()
+    {
+        float p = frac(sampleIndex++* .1031);
+        p *= p + 33.33;
+        p *= p + p;
+        return frac(p + randOffset);
+    }
+
+    void Update(float3 inputDir, float targetWeight, float sampleWeight) {
+        sampleWeight *= targetWeight;
+        M++;
+        wSum += sampleWeight;
+        if (rand() < sampleWeight / max(1e-4, wSum)) 
+        {
+            dir = inputDir;
+            w = targetWeight;
+        }
+    }    
+
+    void Merge(Reservoir r)
+    {
+        if (r.M == 0 || r.w == 0 || r.W_sum == 0) return;
+        wSum += r.wSum;
+        if (rand() < re.wSum / max(1e-4, wSum))
+        {
+            dir = r.dir;
+            w = r.w;
+        }
+
+        M += other.M;
+    }
+}
+
 #endif // KAISER_RAYTRACING_RESERVOIR
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Kaiser-Ray-Tracer
+
+Kaiser-Ray-Tracer is a hybrid render pipeline based on Unity SRP.
+
