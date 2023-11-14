11a320dadccfdcfe5f1fd0c18c9734ce99efc11b
10H-He0 <2139338089@qq.com>
2023-11-14 21:14:06 +0800

    update ircache

---
+++ Assets/Kaiser RP/Include/BVH.hlsl
@@ -0,0 +1,11 @@
+#ifndef BVH_HLSL
+#define BVH_HLSL
+
+// Raytracing acceleration structure, accessed as a SRV
+RaytracingAccelerationStructure acceleration_structure : register(t0, space1);
+
+#endif
+
+
+
+
